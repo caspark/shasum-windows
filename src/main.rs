6dd80923ec93aea8dc57c5f6ed6eba513b58957e
Caspar Krieger <git@asparck.com>
2020-05-17 17:56:27 -0700

    tolerate IO copying errors

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,10 @@ fn unixy_filename_of(entry: &walkdir::DirEntry) -> String {
     return entry.path().display().to_string().replace("\\", "/");
 }
 
+fn output_file_err(path: &str, err: &str) {
+    println!("Error:{err}  {path}", err = err, path = path);
+}
+
 fn main() {
     let args: Vec<_> = env::args().collect();
     if args.len() != 2 {
@@ -20,32 +24,35 @@ fn main() {
     for entry in WalkDir::new(root).same_file_system(true) {
         let entry = entry.expect("able to read file");
         if entry.file_type().is_file() {
+            let path = unixy_filename_of(&entry);
             match fs::File::open(entry.path()) {
                 Ok(mut file) => {
                     let mut hasher = Sha1::new();
-                    io::copy(&mut file, &mut hasher).expect("io copying should work");
-                    let result = hasher.result();
-                    println!(
-                        "{hash:x}  {path}",
-                        hash = result,
-                        path = unixy_filename_of(&entry)
-                    );
+                    if let Err(err) = io::copy(&mut file, &mut hasher) {
+                        let err_msg = format!("{err:?}", err = err);
+                        output_file_err(&path, &err_msg);
+                    } else {
+                        let result = hasher.result();
+                        println!(
+                            "{hash:x}  {path}",
+                            hash = result,
+                            path = unixy_filename_of(&entry)
+                        );
+                    }
                 }
                 Err(err) => {
-                    let path = unixy_filename_of(&entry);
-
                     let tip = if path.len() > 260 {
                         // path is longer than normal windows limit - long path support might need to be enabled
                         Some(Cow::from(format!("NB: path length of {len} is greater than 260 limit; you might need to enable the 'Enable Win32 long paths' group policy setting", len = path.len())))
                     } else {
                         None
                     };
-                    println!(
-                        "Error:{err:?}{tip}  {path}",
+                    let err_msg = format!(
+                        "{err:?}{tip}",
                         err = err,
-                        tip = tip.unwrap_or(Cow::from("")),
-                        path = &path
-                    )
+                        tip = tip.unwrap_or(Cow::from(""))
+                    );
+                    output_file_err(&path, &err_msg);
                 }
             }
         }
